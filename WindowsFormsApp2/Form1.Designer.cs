194e38352b71fe40d9d925749d2ae39411ace111
bon4845 <jaeseo4845@naver.com>
2018-11-01 16:30:19 +0900

    부호를 바꾸었습니다!

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -212,7 +212,7 @@
             this.button13.Name = "button13";
             this.button13.Size = new System.Drawing.Size(49, 35);
             this.button13.TabIndex = 13;
-            this.button13.Text = "/";
+            this.button13.Text = "÷";
             this.button13.UseVisualStyleBackColor = true;
             this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
@@ -224,7 +224,7 @@
             this.button14.Name = "button14";
             this.button14.Size = new System.Drawing.Size(49, 43);
             this.button14.TabIndex = 14;
-            this.button14.Text = "*";
+            this.button14.Text = "×";
             this.button14.UseVisualStyleBackColor = true;
             this.button14.Click += new System.EventHandler(this.button14_Click);
             // 
